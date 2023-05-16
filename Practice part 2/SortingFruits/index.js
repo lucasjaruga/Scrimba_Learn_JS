8a8049e6febf7a74e79eea91bc1578ec5ffd749b
Lucas Jaruga <lucas.jaruga@outlook.com>
2023-05-17 00:37:36 +0200

    SortingFruits solution

---
+++ Practice part 2/SortingFruits/index.js
@@ -0,0 +1,23 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊", "🍊", "🍊", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+
+
+/**
+ * @param arrayWithFruits - array which stores fruits to sort
+ */
+function sortFruits(arrayWithFruits){
+    for(let i = 0; i < fruit.length; i++) {
+        if(fruit[i] === "🍎"){
+            appleShelf.textContent += fruit[i]
+        } else {
+            orangeShelf.textContent += fruit[i]
+        }
+    }
+}
+
+sortFruits()
