62f276259f784de0cc16e1bd11af97e048344e18
Lucas Jaruga <lucas.jaruga@outlook.com>
2023-05-16 22:14:44 +0200

    ObjectsAndFunctions solution

---
+++ Practice part 2/ObjectsAndFunctions.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Lucas",
+    age: 33,
+    country: "Poland",
+    logData: function(){
+        console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+    }
+}
+
+
+person.logData()
