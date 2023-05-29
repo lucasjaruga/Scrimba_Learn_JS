cde934af57757804d2e76c9d612e1919856a659a
Lucas Jaruga <lucas.jaruga@outlook.com>
2023-05-29 20:47:20 +0200

    cleaning a code for deploying

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -13,16 +13,9 @@ saveButton.addEventListener("click", function (){
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `<li>
                         <a target='_blank' href='${myLeads[i]}'>"${myLeads[i]}"</a>
                       </li>`
     }
     ulEl.innerHTML = listItems
 }
-
-
-
-// const li = document.createElement("li")
-// li.textContent = myLeads[i]
-// ulEl.append(li)
